c56dff045b202b72758e3512cabc6ed8d4dd59c1
Ofer Shany <ofer.shany@cellwize.com>
2018-06-21 19:12:48 +0300

    testing assets

--- test1/mongosetup.sh
+++ test1/mongosetup.sh
@@ -1,6 +1,6 @@
 docker run -d --name mongo mongo:latest /usr/bin/mongod --smallfiles
-chmod a+x ~/bash_aliases
+chmod a+x /home/scrapbook/tutorial/bash_aliases
 #echo 'alias mongo="docker exec -it mongo mongo"' > ~/.bash_aliases
 #chmod a+x ~/.bash_aliases
-. ~/bash_aliases
+. /home/scrapbook/tutorial/bash_aliases
 echo "DDDDDD" > /tmp/test.txt
