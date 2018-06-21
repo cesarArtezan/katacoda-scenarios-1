6ac0dc256e26a9806d9e5baa3db3f14db92abe9b
Ofer Shany <ofer.shany@cellwize.com>
2018-06-21 20:17:43 +0300

    Mongo wait inprovments

--- test1/mongowait.sh
+++
@@ -1,8 +0,0 @@
-echo "Waiting for MongoDB container to be ready. Please hold."
-while [ ! "$(docker ps -q -f name=mongo)" ]
-do
-	sleep 1
-done
-
-echo "Ok, let's go. Configuraing an alias for mongo shell"
-alias mongo="docker exec -it mongo mongo"
