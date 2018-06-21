6c8ad2a5ea4d0804b9b3968f1efd32fbf20683e7
Ofer Shany <ofer.shany@cellwize.com>
2018-06-21 17:56:56 +0300

    improving mongo init script

--- test1/mongosetup.sh
+++ test1/mongosetup.sh
@@ -1,5 +1,5 @@
 docker run -d --name mongo mongo:latest /usr/bin/mongod --smallfiles
 
-echo 'alias mongo="docker exec -it mongo mongo"' > ~/.bashrc
+echo 'alias mongo="docker exec -it mongo mongo"' > ~/.bash_aliases
 #chmod a+x ~/.bash_aliases
 #. ~/.bash_aliases
